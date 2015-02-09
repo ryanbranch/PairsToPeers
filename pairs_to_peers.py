92a6ac80695f851ff5f286ee94c1964a8e1e12d8
rjbranch <rjbrjb@umich.edu>
2015-02-08 23:23:36 -0500

    Error and Formatting fixing, basic decks
    Some formatting errors were fixed, including the standardization of
    using tab characters for indentation.  A few syntactical errors, such as
    the lack of capitalization of "True" or "False" were fixed.
    Basic deck building functionality was added.

--- pairs_to_peers.py
+++ pairs_to_peers.py
@@ -24,121 +24,167 @@ pygame.display.set_caption("Pairs to Peers")
 
 #This class defines the players of the game
 class Player:
-    
-    #MEMBER VARIABLES#
-        # isHuman - a boolean denoting whether the player is a human (true) or an AI (false)
-        # playerName - a string of the name of the player
-        # playerNum - the integer value denoting the player's number (1 = Player 1, 2 = Player 2, etc.)
-        # score - the integer value of the player's score
-        # handArray - array that stores the cards in a player's hand
-    
-    #Constructor Method
-    def __init__(self, name, number, human):
-        self.isHuman = human
-        self.playerName = name
-        self.playerNum = number
-        self.score = 0 #Players are initialized with 0 points
+	
+	#MEMBER VARIABLES#
+		# isHuman - a boolean denoting whether the player is a human (true) or an AI (false)
+		# playerName - a string of the name of the player
+		# playerNum - the integer value denoting the player's number (1 = Player 1, 2 = Player 2, etc.)
+		# score - the integer value of the player's score
+		# handArray - array that stores the cards in a player's hand
+	
+	#Constructor Method
+	def __init__(self, name, number, human):
+		self.isHuman = human
+		self.playerName = name
+		self.playerNum = number
+		self.score = 0 #Players are initialized with 0 points
 
-    #This method is used to increase a player's score by a given number of points
-    def addPoints(self, pointsToAdd):
-        self.score += pointsToAdd
-        
-    def dealAns(self):
-        cardCount = 0
-        while(cardCount < 5):
-            randNum = random.randint(0, totalnumberofcards)
-            if(Answer.beenDealt?? == false){
-                handArray[cardCount] = Answer(randNum)
-                cardCount = cardCount + 1
-                
+	#This method is used to increase a player's score by a given number of points
+	def addPoints(self, pointsToAdd):
+		self.score += pointsToAdd
+		
+	def dealAns(self):
+		cardCount = 0
+        #NOTE: Commented out these lines for now until more of a framework is built around this functionality
+		#while(cardCount < 5):
+			#randNum = random.randint(0, totalnumberofcards)
+			#if(Answer.beenDealt == False)
+				#handArray[cardCount] = Answer(randNum)
+				#cardCount = cardCount + 1
 
 #This class defines the scenario cards, which players consider when playing their corresponding action card.
 class Scenario:
-    
-    #MEMBER VARIABLES#
-        # scenarioText - a string describing the scenario
-        # beenPlayed - a boolean that keeps track of whether or not the scenario has already been in play during the current game
-    
-    #Constructor Method
-    def __init__(self, cardText):
-        self.scenarioText = cardText
-        self.beenPlayed = False #Scenario cards are initialized having not been played
+	
+	#MEMBER VARIABLES#
+		# scenarioText - a string describing the scenario
+		# beenPlayed - a boolean that keeps track of whether or not the scenario has already been in play during the current game
+	
+	#Constructor Method
+	def __init__(self, cardText):
+		self.scenarioText = cardText
+		self.beenPlayed = False #Scenario cards are initialized having not been played
 
-    #This method is used to denote that the scenario card has now been played
-    def play(self):
-        self.beenPlayed = True
+	#This method is used to denote that the scenario card has now been played
+	def play(self):
+		self.beenPlayed = True
 
 class Answer:
 
-      #MEMBER VARIABLES#
-        # ansText - a string describing the scenario
-        # pointVal - an int that will keep track of how many points playing this card will give. Changes based on scenario
-        # beenDealt - a boolean that keeps track of whether or not an answer card has been dealt to a player already
-        # beenPlayed - a boolean that keeps track of whether or not the scenario has already been in play during the current game
-        #NOTE: that beenPlayed may or may not be optional for this since we haven't decided if we want to recycle cards yet
-  
-  #cons      
-  def __init__(self, cardText):
-    self.ansText = cardText
-    self.beenDealt = false
-    self.beenPlayed = false
-  
-  #call this to change the number of points an answer is worth each round
-  def setPoints(self, points):
-    self.pointVal = points
-  
+	#MEMBER VARIABLES#
+		# ansText - a string describing the scenario
+		# pointVal - an int that will keep track of how many points playing this card will give. Changes based on scenario
+		# beenDealt - a boolean that keeps track of whether or not an answer card has been dealt to a player already
+		# beenPlayed - a boolean that keeps track of whether or not the scenario has already been in play during the current game
+		#NOTE: that beenPlayed may or may not be optional for this since we haven't decided if we want to recycle cards yet
+	
+	#cons		
+	def __init__(self, cardText):
+		self.ansText = cardText
+		self.beenDealt = False
+		self.beenPlayed = False
+
+	#call this to change the number of points an answer is worth each round
+	def setPoints(self, points):
+		self.pointVal = points
+	
 	#NOTE: Let's add some sort of description for this method.
-  def playCard(self):
-    self.beenPlayed = true
-    #following line incorrect since scoring system hasn't been started yet
-    #pointsOfPlayer = pointsofPlayer + pointVal
+	def playCard(self):
+		self.beenPlayed = True
+		#following line incorrect since scoring system hasn't been started yet
+		#pointsOfPlayer = pointsofPlayer + pointVal
+
+#NOTE: Porentially down the line we should have the scenarios and answer cards read in from files instead of having tons of lines in the program to handle it
+#This function builds the deck of scenario cards.  More lines can be added accordingly whenever more cards are to be added.
+def buildScenarios():
+	scenarios = []
+	card = Scenario("A person walks up to you on the first day of school and says hello.")
+	scenarios.append(card)
+	card = Scenario("Your friend is crying.")
+	scenarios.append(card)
+	card = Scenario("You are hungry.")
+	scenarios.append(card)
+	card = Scenario("You are looking for a pencil but can't find one")
+	scenarios.append(card)
+	card = Scenario("PLACEHOLDER SCENARIO")
+	scenarios.append(card)
+	return scenarios
+	
+#This function builds the deck of answer cards.  More lines can be added accordingly whenever more cards are to be added.
+def buildAnswers():
+	answers = []
+	card = Answer("Say you're sorry")
+	answers.append(card)
+	card = Answer("Ask for help")
+	answers.append(card)
+	card = Answer("Turn around and walk away")
+	answers.append(card)
+	card = Answer("Eat some food")
+	answers.append(card)
+	card = Answer("Look around")
+	answers.append(card)
+	card = Answer("Clap your hands")
+	answers.append(card)
+	card = Answer("Yell at them")
+	answers.append(card)
+	card = Answer("Say hello")
+	answers.append(card)
+	card = Answer("Introduce yourself")
+	answers.append(card)
+	card = Answer("Ask for their name")
+	answers.append(card)
+	card = Answer("PLACEHOLDER ANSWER")
+	answers.append(card)
+	return answers
 		
 #This function takes a string, a color, and a coordinate as input and displays text on the screen accordingly
 def displayMessage(messageText,messageColor,messageLocation):
-    screen_text = FONT.render(messageText, True, messageColor)
-    gameDisplay.blit(screen_text, messageLocation)
+	screen_text = FONT.render(messageText, True, messageColor)
+	gameDisplay.blit(screen_text, messageLocation)
 
-#This is the main function of the program.  It handles everything that's going on at each moment of the game
+#This is the main function of the program.	It handles everything that's going on at each moment of the game
 def gameLoop():
-    
-    gameRun = True #Boolean that stores whether the game should be running
-    gameOver = False #Boolean that stores whether the player has lost
-    clock = pygame.time.Clock()
-    playerArray = []#Initializes the array that will eventually store the players of the game
-    scenarioArray = []#Initializes the array that will eventually store the scenario cards
-    
-    while gameRun: #Continues to execute until gameRun is set to false
-        
-        while gameOver: #Executes after the game has ended
-            gameDisplay.fill(COLOR_BLACK)
-            displayMessage("GAME OVER!  Press ENTER to play again, or SPACE to quit.",COLOR_RED,[GAME_WIDTH/3,GAME_HEIGHT/2])
-            pygame.display.update()
-            
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RETURN:
-                        gameLoop() #Restarts the game if the player presses the ENTER key
-                    if event.key == pygame.K_SPACE:
-                        gameRun = False #Closes the game if the player presses the SPACE key
-                        gameOver = False
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                gameRun = False #Ends the game if they user attempts to close the window
-            #Handles events when a key is pressed
-            if event.type == pygame.KEYDOWN:
-                    
-                #This line is just here as a placeholder for future keyboard events to be added
-                if event.key == pygame.K_RETURN:
-                    gameOver = True
-                
-        gameDisplay.fill(COLOR_WHITE) #Creates a white background
-        displayMessage("The game is running.  Press ENTER to go to the Game Over screen.",COLOR_BLACK,[32,32]) #Draws the score
-        pygame.display.update() #Updates the screen every frame
-        
-        clock.tick(FRAMES_PER_SECOND)
-    
-    pygame.quit()
-    quit()
-    
+	
+	gameRun = True #Boolean that stores whether the game should be running
+	gameOver = False #Boolean that stores whether the player has lost
+	clock = pygame.time.Clock()
+	playerArray = []#Initializes the array that will eventually store the players of the game
+	scenarioArray = []#Initializes the array that will eventually store the scenario cards
+	
+	scenarioArray = buildScenarios()
+	answerArray = buildAnswers()
+	
+	while gameRun: #Continues to execute until gameRun is set to false
+		
+		while gameOver: #Executes after the game has ended
+			gameDisplay.fill(COLOR_BLACK)
+			displayMessage("GAME OVER!	Press ENTER to play again, or SPACE to quit.",COLOR_RED,[GAME_WIDTH/3,GAME_HEIGHT/2])
+			pygame.display.update()
+			
+			for event in pygame.event.get():
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_RETURN:
+						gameLoop() #Restarts the game if the player presses the ENTER key
+					if event.key == pygame.K_SPACE:
+						gameRun = False #Closes the game if the player presses the SPACE key
+						gameOver = False
+		
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				gameRun = False #Ends the game if they user attempts to close the window
+			#Handles events when a key is pressed
+			if event.type == pygame.KEYDOWN:
+					
+				#This line is just here as a placeholder for future keyboard events to be added
+				if event.key == pygame.K_RETURN:
+					gameOver = True
+				
+		gameDisplay.fill(COLOR_WHITE) #Creates a white background
+		displayMessage("The game is running.  Press ENTER to go to the Game Over screen.",COLOR_BLACK,[32,32]) #Draws the score
+		pygame.display.update() #Updates the screen every frame
+		
+		clock.tick(FRAMES_PER_SECOND)
+	
+	pygame.quit()
+	quit()
+	
 gameLoop()
