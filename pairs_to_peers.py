acdda1b5177266dd2dc4818908631a7b27793998
rjbranch <rjbrjb@umich.edu>
2015-02-16 23:38:05 -0500

    Card display and basic dealing
    The game now effectively starts in the middle of a game (although turns
    do not progress and no action can really be taken).  The user's 5
    accessible action cards now display at the bottom of the screen, with
    shuffling so that different actions are displayed each time.
    
    There's also a ton of files being committed due to some stuff being
    moved around, some stuff being renamed, and some stuff being deleted.

--- pairs_to_peers.py
+++ pairs_to_peers.py
@@ -3,6 +3,7 @@
 
 import pygame
 import random
+from textrect import *
 
 pygame.init()
 #In the following section, a number of constants are defined.
@@ -18,10 +19,25 @@ GAME_HEIGHT = 768
 FRAMES_PER_SECOND = 30
 ANSWERS_PER_PLAYER = 2 #The number of answer cards that each player will have at any given time.  Currently set to 2 just because I don't have many answer cards written.
 FONT = pygame.font.SysFont(None, 25)
+ANSWER_CARD_FONT = pygame.font.Font('fonts/OpenSans-Regular.ttf', 18)
+
+#The rest of these constants relate specifically to the locations of images on the game screen.  Tweaking with these could definitely mess up how everything looks.
+POS_ANSWER1 = (190,570)
+POS_ANSWER2 = (325,570)
+POS_ANSWER3 = (460,570)
+POS_ANSWER4 = (595,570)
+POS_ANSWER5 = (730,570)
 
 #These two lines create the window in which the game is played, titling it "Pairs to Peers".
 gameDisplay = pygame.display.set_mode((GAME_WIDTH,GAME_HEIGHT))
 pygame.display.set_caption("Pairs to Peers")
+
+spr_answerCard1 = pygame.image.load('img/answerCard_blue.png')
+spr_answerCard2 = pygame.image.load('img/answerCard_blue.png')
+spr_answerCard3 = pygame.image.load('img/answerCard_blue.png')
+spr_answerCard4 = pygame.image.load('img/answerCard_blue.png')
+spr_answerCard5 = pygame.image.load('img/answerCard_blue.png')
+answerSpriteArray = [spr_answerCard1, spr_answerCard2, spr_answerCard3, spr_answerCard4, spr_answerCard5]
 
 #This class defines the players of the game
 class Player:
@@ -39,6 +55,7 @@ class Player:
 		self.playerName = name
 		self.playerNum = number
 		self.score = 0 #Players are initialized with 0 points
+		self.handArray = []#Initializes handArray as an empty array
 
 	#This method is used to increase a player's score by a given number of points
 	def addPoints(self, pointsToAdd):
@@ -53,7 +70,7 @@ class Player:
 				#handArray[cardCount] = Answer(randNum)
 				#cardCount = cardCount + 1
 
-#This class defines the scenario cards, which players consider when playing their corresponding action card.
+#This class defines the scenario cards, which players consider when playing their corresponding answer card.
 class Scenario:
 
 	#MEMBER VARIABLES#
@@ -112,7 +129,13 @@ def buildScenarios():
 	scenarios.append(card)
 	card = Scenario("You are looking for a pencil but can't find one")
 	scenarios.append(card)
-	card = Scenario("PLACEHOLDER SCENARIO")
+	card = Scenario("There is a bully, saying mean things, who won't leave you alone.")
+	scenarios.append(card)
+	card = Scenario("You notice that someone in your class has candy and you want some.")
+	scenarios.append(card)
+	card = Scenario("You are offered candy by a friend.")
+	scenarios.append(card)
+	card = Scenario("another scenario goes here")
 	scenarios.append(card)
 	return scenarios
 
@@ -139,7 +162,31 @@ def buildAnswers():
 	answers.append(card)
 	card = Answer("Ask for their name")
 	answers.append(card)
-	card = Answer("PLACEHOLDER ANSWER")
+	card = Answer("Nod your head")
+	answers.append(card)
+	card = Answer("Shrug your shoulders")
+	answers.append(card)
+	card = Answer("Do nothing")
+	answers.append(card)
+	card = Answer("Call 911")
+	answers.append(card)
+	card = Answer("Offer to help")
+	answers.append(card)
+	card = Answer("Ask for help")
+	answers.append(card)
+	card = Answer("Give them a hug")
+	answers.append(card)
+	card = Answer("Offer them tissues")
+	answers.append(card)
+	card = Answer("Run away")
+	answers.append(card)
+	card = Answer("Say please")
+	answers.append(card)
+	card = Answer("Say thank you")
+	answers.append(card)
+	card = Answer("Congratulate them")
+	answers.append(card)
+	card = Answer("another answer goes here")
 	answers.append(card)
 	return answers
 
@@ -151,6 +198,12 @@ def displayMessage(messageText,messageColor,messageLocation):
 #This function takes some text and the coordinates of a button rectangle and places text on the button appropriately.
 def buttonText(text, color, xPos, yPos, width, height, size):
 	return 0 #Not exactly sure if this is how we want to do it, but if so, add to this later.
+
+#This function is used to handle everything that's going on during a player's turn.
+def doTurn(thePlayer, answerDeck):
+	cardsInHand = len(thePlayer.handArray)
+	for x in xrange(0, (5 - cardsInHand)): #Iterates until the user's hand is full
+		thePlayer.handArray.append(answerDeck.pop()) #This is effectively dealing a card, as it removes the last element from the answer deck and palces it in the player's hand
 
 #This is the main function of the program.	It handles everything that's going on at each moment of the game
 def gameLoop():
@@ -164,10 +217,15 @@ def gameLoop():
 	answerArray = buildAnswers()
 	scenarioArray = shuffle(scenarioArray)
 	answerArray = shuffle(answerArray)
+	answerRects = [pygame.Rect(POS_ANSWER1[0] + 10, POS_ANSWER1[1] + 10, 108, 150),
+				   pygame.Rect(POS_ANSWER2[0] + 10, POS_ANSWER2[1] + 10, 108, 150),
+				   pygame.Rect(POS_ANSWER3[0] + 10, POS_ANSWER3[1] + 10, 108, 150),
+				   pygame.Rect(POS_ANSWER4[0] + 10, POS_ANSWER4[1] + 10, 108, 150),
+				   pygame.Rect(POS_ANSWER5[0] + 10, POS_ANSWER5[1] + 10, 108, 150)]
 
 	while gameRun: #Continues to execute until gameRun is set to false
 
-		"""  Commenting this out until beta.
+		"""  Commenting this out until beta.  It will actually be it's own file though most likely.
 		#PLAYER SELECTION SCREEN
 		#For some reason it appears that the game is stuck in an unresponsive state during this time.  Definitely need to fix this.
 		while not playersIn: #name, number, human
@@ -207,6 +265,15 @@ def gameLoop():
 
 		gameDisplay.fill(COLOR_WHITE) #Creates a white background
 		displayMessage("The game is running.  Press ENTER to go to the Game Over screen.",COLOR_BLACK,[32,32]) #Draws some text
+		gameDisplay.blit(spr_answerCard1, POS_ANSWER1)
+		gameDisplay.blit(spr_answerCard2, POS_ANSWER2)
+		gameDisplay.blit(spr_answerCard3, POS_ANSWER3)
+		gameDisplay.blit(spr_answerCard4, POS_ANSWER4)
+		gameDisplay.blit(spr_answerCard5, POS_ANSWER5)
+		for x in xrange(0,5):
+			cardRendered = render_textrect(answerArray[x].ansText, ANSWER_CARD_FONT, answerRects[x], COLOR_BLACK, COLOR_WHITE)
+			if cardRendered:
+				gameDisplay.blit(cardRendered, answerRects[x].topleft)
 		pygame.display.update() #Updates the screen every frame
 
 		clock.tick(FRAMES_PER_SECOND)
